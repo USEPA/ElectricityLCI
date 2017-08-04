b31fbf3bf673b54dca1965076f339362ef7b4f06
Tapajyoti Ghosh <tapajyotighosh007@gmail.com>
2017-08-04 16:14:10 -0600

    Final EGRID database compilation
    This contains the final script files for compiling the eGRID database.

---
+++ eGRIDdatabasegen/eGRIDmorethan10%/egridArranger.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 14:51:34 2017
+
+@author: tghosh
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 22:50:07 2017
+
+@author: Tapajyoti
+"""
+
+import pandas as pd
+import numpy as np
+
+
+
+df1 = pd.read_csv("Aggregated_LCI1.csv",error_bad_lines=False)
+df2 = pd.read_csv("Aggregated_LCI2.csv",error_bad_lines=False)
+df3 = pd.read_csv("Aggregated_LCI3.csv",error_bad_lines=False)
+df4 = pd.read_csv("Aggregated_LCI4.csv",error_bad_lines=False)
+
+
+df1 = pd.melt(df1, id_vars=[' eGRID subregion acronym ',' Plant primary fuel '], 
+            value_vars=list(df1.columns[3:]), 
+            var_name='Emissions', 
+            value_name='Emissions quantity kg/MWh')
+df2 = pd.melt(df2, id_vars=[' eGRID subregion acronym ',' Plant primary coal/oil/gas/ other fossil fuel category '], 
+            value_vars=list(df2.columns[3:]), 
+            var_name='Emissions', 
+            value_name='Emissions quantity kg/MWh')
+df3 = pd.melt(df3, id_vars=[' eGRID subregion acronym ','Solar type'], 
+            value_vars=list(df3.columns[3:]), 
+            var_name='Emissions', 
+            value_name='Emissions quantity kg/MWh')
+df4 = pd.melt(df4, id_vars=[' eGRID subregion acronym ','Geo type'], 
+            value_vars=list(df4.columns[3:]), 
+            var_name='Emissions', 
+            value_name='Emissions quantity kg/MWh')
+
+
+
+df1.to_csv('FinalLCIforimport1.csv')
+df2.to_csv('FinalLCIforimport2.csv')
+df3.to_csv('FinalLCIforimport3.csv')
+df4.to_csv('FinalLCIforimport4.csv')
