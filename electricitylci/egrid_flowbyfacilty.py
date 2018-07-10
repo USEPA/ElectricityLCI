f861b1aab592bc01d5986515f93d7e5158dc41ef
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-10 13:42:58 -0400

    Gets egrid flowbyfacility data from StEWI

---
+++ electricitylci/egrid_flowbyfacilty.py
@@ -0,0 +1,11 @@
+import stewi
+from electricitylci.globals import egrid_year
+
+#Get inventory data to get net generation per facility
+egrid_flowbyfacility = stewi.getInventory("eGRID",egrid_year)
+
+#Peek at it
+egrid_flowbyfacility.head(50)
+
+
+
