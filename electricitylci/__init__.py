0e93ebc5252a53c896b69ef85838666f176999f5
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-26 13:24:39 -0400

    Add two functions to get process and process mix dataframes

--- electricitylci/__init__.py
+++ electricitylci/__init__.py
@@ -1,6 +1,18 @@
 
 
-#create_generation_processes(source='egrid', regions='all')
+def get_generation_process_df(source='egrid', regions='all'):
+    from electricitylci.egrid_filter import electricity_for_selected_egrid_facilities,emissions_and_waste_for_selected_egrid_facilities
+    from electricitylci.egrid_generation_database_builder import create_generation_process_df
+    generation_process_df = create_generation_process_df(electricity_for_selected_egrid_facilities,emissions_and_waste_for_selected_egrid_facilities,subregion=regions)
+    return generation_process_df
 
-#create_generation_mix_processes (source='egrid')
+def get_generation_mix_process_df(source='egrid',regions='all'):
+    from electricitylci.egrid_filter import electricity_for_selected_egrid_facilities
+    from electricitylci.egrid_generation_database_builder import create_generation_mix_process_df
+    generation_mix_process_df = create_generation_mix_process_df(electricity_for_selected_egrid_facilities)
+    return generation_mix_process_df
+
+#write_generation_templates()
+#from electricitylci.egrid_template_builder import *
+#gen_mix_template_generator(generation_mix_dict)
 
