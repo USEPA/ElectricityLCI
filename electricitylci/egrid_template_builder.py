6d5c9962c4aaad9d19ca47da15f0b83a929cb68f
ERG\TGhosh <Tapajyoti.Ghosh@erg.com>
2018-07-27 16:47:34 -0400

    Downstream templates for Canada Mexico

--- electricitylci/egrid_template_builder.py
+++ electricitylci/egrid_template_builder.py
@@ -777,3 +777,154 @@ def surplus_pool_mix_template_generator(surplus_pool_dict,nerc_region):
                         data_dir = os.path.dirname(os.path.realpath(__file__))+"/output/"
                         wb.save(data_dir+filename)
                         print(filename+' File written Successfully')
+                        
+                        
+                        
+def trade_mix_template_generator(trade_dict):
+         data_dir = os.path.dirname(os.path.realpath(__file__))   
+        os.chdir(data_dir+'/data/')            
+         
+        
+    
+        for Reg in trade_dict.keys():
+                        
+                            
+                        wb = openpyxl.load_workbook('template.xlsx')
+                        
+                        gi = wb['General information']
+                        io = wb['InputsOutputs']
+                        
+                        
+                        global blank_row;
+                                   
+                        blank_row = 6;
+                        Column_number = 43 #THis is based on the template number of columns in the template. 
+                           
+                        
+                        index = 1; #Starting the index at a specific point
+                        #REading the list of fuel for the entered region
+                        
+                        
+                                       
+                        
+                        #This block is used for filling up the General Infomration Sheet
+                        
+                        
+                                
+                        
+                        
+                        
+                        gi['D4'].value = 'Electricity '
+                        
+                        gi['D5'].value = 'for trade';
+                        
+                        gi['D6'].value = 'at region '+ Reg;
+                        
+                        gi['D7'].value = 'trade Mix';
+                        
+                        gi['D10'].value = 'Trade mix electricity in the '+Reg+' region'
+                        
+                        
+                        gi['D11'].value = trade_dict[Reg]['category']
+                        
+                        gi['D12'].value = 'FALSE'
+                        
+                        gi['D14'].value = 'Electricity; voltage?'
+                        
+                        gi['D16'].value = trade_dict[Reg]['processDocumentation']['validFrom']
+                        
+                        gi['D17'].value = trade_dict[Reg]['processDocumentation']['validUntil']
+                        
+                        gi['D18'].value = trade_dict[Reg]['processDocumentation']['validFrom']
+                        
+                        gi['D20'].value = Reg
+                        
+                        v= '' ;
+                        #THis function is used for finding the NERC regions. THese input files are built manually. 
+            
+                        
+                        gi['D24'].value = 'Trade from Outside US'
+                        #This is for printing the Prime Movers. Written to a output file and reread back. 
+                        #def pm(fn,rg):
+                        #    f = open('out.txt', 'w')
+                        #    for row in primemover.itertuples():
+                        ##      if row[2] == rg:
+                         #       if row[3]  == fn or row[4] == fn or row[5] == fn or row[6] == fn:
+                        #          if row[7] != None:                   
+                        #             f.write(row[7])
+                        #             f.write(',')
+                        #    f.close()        
+                        
+                         
+                        
+                        
+                        #linestring = open('out.txt', 'r').read()
+                        
+                        gi['D26'].value = 'This is the trade Mix.'
+                        
+                        filename = 'trade Mix '+ Reg + '.xlsx'
+                        
+                        
+                        
+                        #This block is used for filling up the InputOutput Sheet ONLY EMISSIONS
+                        
+                                               
+                        
+                        
+                        #Reading All the files
+                        #io['D5'].value =  Reg+' Distribution Mix electricity';   
+                        
+                        #This function is necessary to creates new line in the template for writing input and output flows. 
+                        def createblnkrow(br):
+                            for i in range(1,Column_number):
+                              
+                              v = io.cell(row = br,column = i).value
+                              io.cell(row = br+1 ,column = i).value = v
+                              io.cell(row = br+1 ,column = i).font = copy(io.cell(row = br,column = i).font)
+                              io.cell(row = br+1 ,column = i).border = copy(io.cell(row = br,column = i).border)
+                              io.cell(row = br+1 ,column = i).fill = copy(io.cell(row = br,column = i).fill)
+                            br = br + 1;
+                              #print(io.cell(row = 6,column = 4).value);
+                            return br
+                        
+                        row = blank_row-1;
+                        
+                         
+                        
+                                               
+                        
+                     
+                         
+                        for index in range(0,len(trade_dict[Reg]['exchanges'])):
+                            
+                            
+                            blank_row = createblnkrow(blank_row)
+                            io[row][0].value = index+1;
+                            
+                            
+                            
+                            if index == 0:
+                               io[row][2].value = 0;
+                               io[row][3].value = trade_dict[Reg]['name']
+                            else:
+                                io[row][1].value = 5;
+                                io[row][3].value = trade_dict[Reg]['exchanges'][index]['flow']['name']
+                            
+                            io[row][4].value = trade_dict[Reg]['category']
+                            io[row][5].value = trade_dict[Reg]['location']
+                            
+                            io[row][6].value = trade_dict[Reg]['exchanges'][index]['amount']
+                            io[row][7].value = 'MWh';
+                            io[row][21].value = trade_dict[Reg]['description']                         
+                            row = row+1;
+                            index=index+1
+                            
+                        
+                                                     
+                         
+                                        
+                             
+                        filename = Reg+"_trade.xlsx"
+                        data_dir = os.path.dirname(os.path.realpath(__file__))+"/output/"
+                        wb.save(data_dir+filename)
+                        print(filename+' File written Successfully')
