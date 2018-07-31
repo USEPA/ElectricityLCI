b261dfc811718e42329c5f092999cdf86879954f
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-31 17:00:57 -0400

    Add flow type mapping based on compartment

--- electricitylci/elementaryflows.py
+++ electricitylci/elementaryflows.py
@@ -17,8 +17,12 @@ def map_emissions_to_fedelemflows(df_with_flows_compartments):
     #If a NewName is present there was a match, replace FlowName and Compartment with new names
     mapped_df.loc[mapped_df['NewName'].notnull(), 'FlowName'] = mapped_df['NewName']
     mapped_df.loc[mapped_df['NewName'].notnull(), 'Compartment'] = mapped_df['NewCategory']
-    mapped_df.loc[mapped_df['NewName'].notnull(), 'Directionality'] = 'emission'
     mapped_df = mapped_df.rename(columns={'NewUnit':'Unit','UUID':'FlowUUID'})
+
+    #If air, soil, or water assigned it directionality of emission. Others will get assigned later as needed
+    emission_compartments = ['air','soil','water']
+    mapped_df.loc[mapped_df['Compartment'].isin(emission_compartments), 'Directionality'] = 'emission'
+
     #Drop all unneeded cols
     mapped_df = mapped_df.drop(columns=['OriginalName','OriginalCategory','OriginalProperty','NewName','NewCategory','NewSubCategory'])
     return mapped_df
@@ -27,28 +31,26 @@ def map_emissions_to_fedelemflows(df_with_flows_compartments):
 #!Still need to consider amount conversion
 def map_renewable_heat_flows_to_fedelemflows(df_with_flows_compart_direction):
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
-                                   (df_with_flows_compart_direction['FuelCategory']=='SOLAR'), 'Directionality'] = 'resource'
+                                   ((df_with_flows_compart_direction['FuelCategory']=='SOLAR')|
+                                    (df_with_flows_compart_direction['FuelCategory'] == 'GEOTHERMAL')|
+                                    (df_with_flows_compart_direction['FuelCategory'] == 'WIND')|
+                                    (df_with_flows_compart_direction['FuelCategory'] == 'HYDRO')), 'Directionality'] = 'resource'
+
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
                                    (df_with_flows_compart_direction['FuelCategory']=='SOLAR'), 'Compartment'] = 'air'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
                                    (df_with_flows_compart_direction['FuelCategory']=='SOLAR'), 'FlowName'] = 'Energy, solar'
 
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
-                                   (df_with_flows_compart_direction['FuelCategory']=='GEOTHERMAL'), 'Directionality'] = 'resource'
-    df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
                                    (df_with_flows_compart_direction['FuelCategory']=='GEOTHERMAL'), 'Compartment'] = 'ground'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
                                    (df_with_flows_compart_direction['FuelCategory']=='GEOTHERMAL'), 'FlowName'] = 'Energy, geothermal'
 
-    df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
-                                   (df_with_flows_compart_direction['FuelCategory']=='WIND'), 'Directionality'] = 'resource'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName'] == 'Heat') &
                                    (df_with_flows_compart_direction['FuelCategory'] == 'WIND'), 'Compartment'] = 'air'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName'] == 'Heat') &
                                    (df_with_flows_compart_direction['FuelCategory'] == 'WIND'), 'FlowName'] = 'Energy, wind'
 
-    df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
-                                   (df_with_flows_compart_direction['FuelCategory']=='HYDRO'), 'Directionality'] = 'resource'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName'] == 'Heat') &
                                    (df_with_flows_compart_direction['FuelCategory'] == 'HYDRO'), 'FlowName'] = 'Energy, hydro'
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName'] == 'Heat') &
@@ -56,3 +58,15 @@ def map_renewable_heat_flows_to_fedelemflows(df_with_flows_compart_direction):
 
     return df_with_flows_compart_direction
 
+#Compartment to flow type mapping
+#Use valid flow types in json_ld http://greendelta.github.io/olca-schema/html/FlowType.html
+compartment_to_flowtype = pd.DataFrame(columns=['Compartment', 'FlowType'], data=[['air', 'ELEMENTARY_FLOW'],
+                                                                                  ['water', 'ELEMENTARY_FLOW'],
+                                                                                  ['ground', 'ELEMENTARY_FLOW'],
+                                                                                  ['input', 'PRODUCT_FLOW'],
+                                                                                  ['output', 'PRODUCT_FLOW'],
+                                                                                  ['waste', 'WASTE_FLOW']])
+
+def map_compartment_to_flow_type(df_with_compartments):
+    df_with_flowtypes = pd.merge(df_with_compartments,compartment_to_flowtype,on=['Compartment'],how='left')
+    return df_with_flowtypes
