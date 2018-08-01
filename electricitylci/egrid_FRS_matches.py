9475f5dc8ae11da14b3de01a4b4bfa9e5cf1f258
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-01 13:52:58 -0400

    Update for changes to facilitymatcher functions and output fields

--- electricitylci/egrid_FRS_matches.py
+++ electricitylci/egrid_FRS_matches.py
@@ -8,26 +8,22 @@ egrid_FRS_matches = facilitymatcher.get_matches_for_inventories(["eGRID"])
 egrid_FRS_matches.head()
 
 #Get NAICS info for inventories we're potentially interested in
-egrid_frs_ids = list(pd.unique(egrid_FRS_matches['REGISTRY_ID']))
-frs_programs_of_interest = facilitymatcher.globals.get_programs_for_inventory_list(inventories)
-egrid_FRS_NAICS = facilitymatcher.getFRSNAICSInfoforFacilityList(egrid_frs_ids,frs_programs_of_interest)
+egrid_frs_ids = list(pd.unique(egrid_FRS_matches['FRS_ID']))
+egrid_FRS_NAICS = facilitymatcher.get_FRS_NAICSInfo_for_facility_list(egrid_frs_ids,inventories)
 
 get_first_4 = lambda x: x[0:4]
-egrid_FRS_NAICS['NAICS_4'] =  egrid_FRS_NAICS['NAICS_CODE'].map(get_first_4)
-
-#egrid_FRS_NAICS.columns
-#egrid_FRS_NAICS.head(50)
+egrid_FRS_NAICS['NAICS_4'] =  egrid_FRS_NAICS['NAICS'].map(get_first_4)
 
 #import egrid_facilities
 egrid_facilities_w_ids_subregions_fuels = egrid_facilities[['FacilityID','Subregion','PrimaryFuel','FuelCategory']]
 #Merge egrid facilities with facility ids
-egrid_facilities_with_FRS = pd.merge(egrid_facilities_w_ids_subregions_fuels,egrid_FRS_matches,left_on='FacilityID',right_on='PGM_SYS_ID',how='left')
+egrid_facilities_with_FRS = pd.merge(egrid_facilities_w_ids_subregions_fuels,egrid_FRS_matches,on='FacilityID',how='left')
 #Drop records with no FRS
-egrid_facilities_with_FRS = egrid_facilities_with_FRS[egrid_facilities_with_FRS['REGISTRY_ID'].notnull()]
+egrid_facilities_with_FRS = egrid_facilities_with_FRS[egrid_facilities_with_FRS['FRS_ID'].notnull()]
 len(egrid_facilities_with_FRS)
 #2016:7042
 
-egrid_facilities_with_FRS_NAICS = pd.merge(egrid_facilities_with_FRS,egrid_FRS_NAICS,on='REGISTRY_ID')
+egrid_facilities_with_FRS_NAICS = pd.merge(egrid_facilities_with_FRS,egrid_FRS_NAICS,on='FRS_ID')
 
 def list_FRS_ids_filtered_for_NAICS():
     egrid_facilities_with_FRS_NAICS_filtered = egrid_facilities_with_FRS_NAICS[((egrid_facilities_with_FRS_NAICS['NAICS_4'] == '5622')
@@ -36,7 +32,7 @@ def list_FRS_ids_filtered_for_NAICS():
                                                                                |
                                                                                ((egrid_facilities_with_FRS_NAICS['NAICS_4'] == '2211')
                                                                                 & (egrid_facilities_with_FRS_NAICS['PRIMARY_INDICATOR'] == 'PRIMARY'))]
-    frs_ids = list(pd.unique(egrid_facilities_with_FRS_NAICS_filtered['REGISTRY_ID']))
+    frs_ids = list(pd.unique(egrid_facilities_with_FRS_NAICS_filtered['FRS_ID']))
     return frs_ids
 
 
@@ -64,9 +60,3 @@ def list_FRS_ids_filtered_for_NAICS():
 #             statement = c + ' or '
 #         else:
 #             statement = statement + c
-
-
-
-#NAICS
-egrid_facilities_with_FRS_NAICSInfo_filtered =  egrid_facilities_with_FRS_NAICS[(egrid_facilities_with_FRS_NAICS['NAICS_4']=='2211')&(egrid_facilities_with_FRS_NAICS['PRIMARY_INDICATOR']=='PRIMARY')]
-
