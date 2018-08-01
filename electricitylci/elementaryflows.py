1664ed369f820531c2d8344f78c1cca04611b13a
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-01 16:20:46 -0400

    Change Directionality to ElementaryFlowPrimeContext Add FlowDirection function

--- electricitylci/elementaryflows.py
+++ electricitylci/elementaryflows.py
@@ -21,7 +21,7 @@ def map_emissions_to_fedelemflows(df_with_flows_compartments):
 
     #If air, soil, or water assigned it directionality of emission. Others will get assigned later as needed
     emission_compartments = ['air','soil','water']
-    mapped_df.loc[mapped_df['Compartment'].isin(emission_compartments), 'Directionality'] = 'emission'
+    mapped_df.loc[mapped_df['Compartment'].isin(emission_compartments), 'ElementaryFlowPrimeContext'] = 'emission'
 
     #Drop all unneeded cols
     mapped_df = mapped_df.drop(columns=['OriginalName','OriginalCategory','OriginalProperty','NewName','NewCategory','NewSubCategory'])
@@ -34,7 +34,7 @@ def map_renewable_heat_flows_to_fedelemflows(df_with_flows_compart_direction):
                                    ((df_with_flows_compart_direction['FuelCategory']=='SOLAR')|
                                     (df_with_flows_compart_direction['FuelCategory'] == 'GEOTHERMAL')|
                                     (df_with_flows_compart_direction['FuelCategory'] == 'WIND')|
-                                    (df_with_flows_compart_direction['FuelCategory'] == 'HYDRO')), 'Directionality'] = 'resource'
+                                    (df_with_flows_compart_direction['FuelCategory'] == 'HYDRO')), 'ElementaryFlowPrimeContext'] = 'resource'
 
     df_with_flows_compart_direction.loc[(df_with_flows_compart_direction['FlowName']=='Heat')&
                                    (df_with_flows_compart_direction['FuelCategory']=='SOLAR'), 'Compartment'] = 'air'
@@ -69,4 +69,10 @@ compartment_to_flowtype = pd.DataFrame(columns=['Compartment', 'FlowType'], data
 
 def map_compartment_to_flow_type(df_with_compartments):
     df_with_flowtypes = pd.merge(df_with_compartments,compartment_to_flowtype,on=['Compartment'],how='left')
+    return df_with_flowtypes
+
+def add_flow_direction(df_with_flowtypes):
+    df_with_flowtypes['FlowDirection'] = 'output'
+    df_with_flowtypes.loc[(df_with_flowtypes['Compartment'] == 'input') |
+                          (df_with_flowtypes['ElementaryFlowPrimeContext'] == 'resource', 'FlowDirection') = 'input'
     return df_with_flowtypes
