f99d1eee8bd4f793b4fef87c7d35e74e868efa5f
ERG\TGhosh <Tapajyoti.Ghosh@erg.com>
2018-08-09 20:12:37 -0400

    Genralzing to all regions

--- electricitylci/egrid_facilities.py
+++ electricitylci/egrid_facilities.py
@@ -15,7 +15,6 @@ egrid_subregions = list(pd.unique(egrid_facilities['Subregion']))
 egrid_subregions = [x for x in egrid_subregions if str(x) != 'nan']
 len(egrid_subregions)
 
-egrid_subregions = ['AZNM','CAMX','NWPP']
 #2016: 26
 
 egrid_primary_fuel_categories = sorted(pd.unique(egrid_facilities['FuelCategory'].dropna()))
