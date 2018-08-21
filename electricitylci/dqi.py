4a3e65e6ffa3307e9d0733596010266be54c55ae
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-21 14:16:21 -0400

    Add flow data quality field list

--- electricitylci/dqi.py
+++ electricitylci/dqi.py
@@ -1,6 +1,7 @@
 #Scoring based on USEPA 2016: Guidance on Data Quality Assessment for Life Cycle Inventory Data
 
-
+flow_data_quality_fields = ['Reliability_Score','TemporalCorrelation','GeographicalCorrelation',
+                            'TechnologicalCorrelation','DataCollection']
 temporal_correlation_lower_bound_to_dqi = {3:1,6:2,10:3,15:4,None:5}
 
 data_collection_lower_bound_to_dqi = {.4:4,.6:3,.8:2,1:1,None:5}
