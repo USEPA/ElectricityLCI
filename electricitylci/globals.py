a02890696202bbdc0146b2911185f968b50722a6
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-28 17:57:55 -0400

    Add model_name and post_processing vars to globals

--- electricitylci/globals.py
+++ electricitylci/globals.py
@@ -31,6 +31,7 @@ min_plant_percent_generation_from_primary_fuel_category = 90
 
 efficiency_of_distribution_grid = 0.95
 
+model_name = 'ELCI_1'
 
 #Reading the fuel name file
 fuel_name = pd.read_csv(data_dir+'fuelname.csv')
@@ -40,6 +41,12 @@ net_trading = False
 
 #Flow list
 fedelemflowlist_version = '0.1'
+
+#Determine whether or not to post-process the generation data
+post_process_generation_emission_factors = True
+
+#Set number of standard deviations to use away from the mean to post-process the gen database
+post_process_generation_emission_factors_sd_limit = 3
 
 #
 def join_with_underscore(items):
@@ -52,3 +59,4 @@ def join_with_underscore(items):
         items = [str(x) for x in items]
 
     return "_".join(items)
+
