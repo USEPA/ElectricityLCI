a18f0fa0db135e1c859b6928e44810721d2a1f5a
msrocka <michael.srocka@gmail.com>
2018-08-29 18:17:46 +0200

    generate category trees

--- electricitylci/olca_jsonld_writer.py
+++ electricitylci/olca_jsonld_writer.py
@@ -1,3 +1,4 @@
+import typing
 import uuid
 
 import olca
@@ -9,13 +10,41 @@ def write(processes: dict, file_path: str):
         given path.
     """
     with pack.Writer(file_path) as writer:
+        created_ids = set()
         for d in processes.values():
             process = olca.Process()
             process.name = _val(d, 'name')
             category_path = _val(d, 'category')
             process.id = _uid(olca.ModelType.PROCESS,
                               category_path, process.name)
-            writer.write(process)
+            process.category = _category(
+                category_path, olca.ModelType.PROCESS, writer, created_ids)
+            # writer.write(process)
+
+
+def _category(path: str, mtype: olca.ModelType, writer: pack.Writer,
+              created_ids: set) -> typing.Optional[olca.Ref]:
+    if not isinstance(path, str):
+        return None
+    if path.strip() == '':
+        return None
+    parts = path.split('/')
+    parent = None  # type: olca.Ref
+    for i in range(0, len(parts)):
+        uid_path = [str(mtype.value)] + parts[0:(i+1)]
+        uid = _uid(*uid_path)
+        name = parts[i].strip()
+        if uid not in created_ids:
+            category = olca.Category()
+            category.id = uid
+            category.model_type = mtype
+            category.name = name
+            category.category = parent
+            writer.write(category)
+            created_ids.add(uid)
+        parent = olca.ref(olca.Category, uid, name)
+        parent.category_path = uid_path[1:]
+    return parent
 
 
 def _val(d: dict, *path, **kvargs):
@@ -38,7 +67,11 @@ def _uid(*args):
 
 # this currently just for testing and will be removed later
 if __name__ == '__main__':
+    import os
     import pickle
+    zip_file = '../ElectricityLCI_jsonld.zip'
+    if os.path.isfile(zip_file):
+        os.remove(zip_file)
     with open('../CAMX_gen_dict_WI.p', 'rb') as f:
-        processes = pickle.load(f)
-        write(processes, '../ElectricityLCI_jsonld.zip')
+        data = pickle.load(f)
+        write(data, zip_file)
