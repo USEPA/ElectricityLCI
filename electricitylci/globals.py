9d92d9fe9a2622df9b01f5e3f7b5fe49003556bb
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-29 14:42:12 -0400

    Move model configuration variables to a separate json file

--- electricitylci/globals.py
+++ electricitylci/globals.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
+import json
 
 def set_dir(directory):
     if not os.path.exists(directory): os.makedirs(directory)
     return directory
-
 
 try: modulepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
 except NameError: modulepath = 'electricitylci/'
@@ -12,41 +12,35 @@ except NameError: modulepath = 'electricitylci/'
 output_dir = modulepath + 'output/'
 data_dir = modulepath + 'data/'
 
-
-#Declare target year for LCI (recommend current year by default) and year of eGRID data to use
-electricity_lci_target_year = 2018
-egrid_year = 2016
-
-#emissions data to include in LCI
-inventories_of_interest = {'eGRID':'2016','TRI':'2016','NEI':'2016','RCRAInfo':'2015'}
-inventories = inventories_of_interest.keys()
-
-#Criteria for filtering eGRID data to include
-include_only_egrid_facilities_with_positive_generation = True
-
-egrid_facility_efficiency_filters = {'lower_efficiency':10,
-                                     'upper_efficiency':100}
-
-min_plant_percent_generation_from_primary_fuel_category = 90
-
-efficiency_of_distribution_grid = 0.95
-
-model_name = 'ELCI_1'
-
 #Reading the fuel name file
 fuel_name = pd.read_csv(data_dir+'fuelname.csv')
 
+#Set model_name here
+model_name = 'ELCI_1'
+
+#pull in model config vars
+try:
+    with open("electricitylci/modelbuilds/"+model_name+"_config.json") as cfg:
+    model_specs = json.load(cfg)
+except FileNotFoundError:
+    print("Model specs not found. Create a model specs file for the model of interest.")
+
+#Declare target year for LCI (recommend current year by default) and year of eGRID data to use
+electricity_lci_target_year = model_specs["electricity_lci_target_year"]
+#Year for egrid data
+global egrid_year
+egrid_year = model_specs["egrid_year"]
+#emissions data to include in LCI
+inventories_of_interest = model_specs["inventories_of_interest"]
+inventories = inventories_of_interest.keys()
+#Criteria for filtering eGRID data to include
+include_only_egrid_facilities_with_positive_generation = model_specs["include_only_egrid_facilities_with_positive_generation"]
 #Trading methodology
-net_trading = False
-
+net_trading = model_specs["net_trading"]
 #Flow list
-fedelemflowlist_version = '0.1'
-
+fedelemflowlist_version = model_specs["fedelemflowlist_version"]
 #Determine whether or not to post-process the generation data
-post_process_generation_emission_factors = True
-
-#Set number of standard deviations to use away from the mean to post-process the gen database
-post_process_generation_emission_factors_sd_limit = 3
+post_process_generation_emission_factors = model_specs["post_process_generation_emission_factors"]
 
 #
 def join_with_underscore(items):
@@ -59,4 +53,5 @@ def join_with_underscore(items):
         items = [str(x) for x in items]
 
     return "_".join(items)
+
 
