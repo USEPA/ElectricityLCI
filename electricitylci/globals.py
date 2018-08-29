9b5caa7cc935c5dbbfde99c233981f94857ecad6
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-29 18:23:02 -0400

    Declare missing vars from config

--- electricitylci/globals.py
+++ electricitylci/globals.py
@@ -16,25 +16,28 @@ data_dir = modulepath + 'data/'
 fuel_name = pd.read_csv(data_dir+'fuelname.csv')
 
 #Set model_name here
-model_name = 'ELCI_1'
+model_name = 'ELCI_2'
 
 #pull in model config vars
 try:
     with open("electricitylci/modelbuilds/"+model_name+"_config.json") as cfg:
-    model_specs = json.load(cfg)
+        model_specs = json.load(cfg)
 except FileNotFoundError:
     print("Model specs not found. Create a model specs file for the model of interest.")
 
 #Declare target year for LCI (recommend current year by default) and year of eGRID data to use
 electricity_lci_target_year = model_specs["electricity_lci_target_year"]
 #Year for egrid data
-global egrid_year
 egrid_year = model_specs["egrid_year"]
 #emissions data to include in LCI
 inventories_of_interest = model_specs["inventories_of_interest"]
 inventories = inventories_of_interest.keys()
 #Criteria for filtering eGRID data to include
+egrid_facility_efficiency_filters = model_specs["egrid_facility_efficiency_filters"]
 include_only_egrid_facilities_with_positive_generation = model_specs["include_only_egrid_facilities_with_positive_generation"]
+min_plant_percent_generation_from_primary_fuel_category = model_specs["min_plant_percent_generation_from_primary_fuel_category"]
+#Global for efficiency
+efficiency_of_distribution_grid = model_specs["efficiency_of_distribution_grid"]
 #Trading methodology
 net_trading = model_specs["net_trading"]
 #Flow list
