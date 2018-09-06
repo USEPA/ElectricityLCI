5d6706b4c3a3ad831b0c6baf78bc0a9a7c6e0209
WesIngwersen <ingwersen.wesley@epa.gov>
2018-09-06 10:41:18 -0400

    Rename and add in national emission total comparison

--- electricitylci/analysis/check_emission_totals_against_egrid_totals.py
+++ electricitylci/analysis/check_emission_totals_against_egrid_totals.py
@@ -32,3 +32,27 @@ subregion_emission_comparison['FracDiff_Model_to_Ref'] = (subregion_emission_com
 #Export result
 subregion_emission_comparison.to_csv(output_dir+model_name+'_egrid_subregion_emission_validation_check.csv',index=False)
 
+
+##Compare US mix to national totals
+#Bring in US result
+USgen = pd.read_csv(output_dir+model_name+'_US_gen_db.csv')
+#drop non-air
+USgen = USgen[USgen['Compartment']=='air']
+#total gen mix
+modelUSgentotals = genmix.groupby('FuelCategory')['Electricity'].sum().reset_index()
+
+USgenwithgenmix = pd.merge(USgen,modelUSgentotals,on=['FuelCategory'])
+USgenwithgenmix['Model_Calculated_Emission']=USgenwithgenmix['Emission_factor']*USgenwithgenmix['Electricity']
+USgenwithgenmix.columns
+#Now sum them by flow name
+model_national_totals = USgenwithgenmix.groupby('FlowName')['Model_Calculated_Emission'].sum().reset_index()
+
+#Now group ref data by Flowname
+ref_national_emission_totals = ref_egrid_subregion_emission_totals.groupby('FlowName')['FlowAmount'].sum().reset_index()
+ref_national_emission_totals = ref_national_emission_totals.rename(columns={'FlowAmount':'Reference_Emission'})
+
+#Merge and compare
+national_emission_comparison = pd.merge(model_national_totals,ref_national_emission_totals,on='FlowName')
+national_emission_comparison['FracDiff_Model_to_Ref'] = (national_emission_comparison['Model_Calculated_Emission']-national_emission_comparison['Reference_Emission'])/national_emission_comparison['Reference_Emission']
+#Export
+national_emission_comparison.to_csv(output_dir+model_name+'_egrid_national_emission_validation_check.csv',index=False)
