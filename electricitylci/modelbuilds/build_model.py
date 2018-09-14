925aabb8c30481d090dc20d8e53957070d87194b
WesIngwersen <ingwersen.wesley@epa.gov>
2018-09-13 22:39:00 -0400

    Commented out backup to retrieve gen db from pickle

--- electricitylci/modelbuilds/build_model.py
+++ electricitylci/modelbuilds/build_model.py
@@ -11,7 +11,10 @@ from electricitylci.globals import output_dir,model_name
 all_generation_db = electricitylci.get_generation_process_df(regions='all')
 len(all_generation_db)
 #Save to pickle for easier loading if desired
-#all_generation_db.to_pickle('work/'+model_name+'all_gen.pk')
+all_generation_db.to_pickle('work/'+model_name+'all_gen.pk')
+#import pandas as pd
+#all_generation_db = pd.read_pickle('work/'+model_name+'all_gen.pk')
+
 #Write it to csv for further analysis or review
 all_generation_db.to_csv(output_dir + model_name+'_all_gen_db.csv',index=False)
 #Write it to a dictionary
