52badc3df7ac7fc1f39741cc8821f121397657f2
Greg Schivley <greg.schivley@gmail.com>
2019-01-08 07:03:46 -0500

    Add util for finding file in folder

--- electricitylci/utils.py
+++ electricitylci/utils.py
@@ -5,6 +5,8 @@ Small utility functions for use throughout the repository
 
 import io
 import zipfile
+import os
+from os.path import join
 
 import requests
 
@@ -29,3 +31,18 @@ def download_unzip(url, unzip_path):
         
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(path=unzip_path)
+
+
+def find_file_in_folder(folder_path, file_pattern_match):
+
+    files = os.listdir(folder_path)
+
+    # would be more elegent with glob but this works to identify the
+    # file in question
+    for f in files:
+        if file_pattern_match in f:
+            file_name = f
+
+    file_path = join(folder_path, file_name)
+
+    return file_path
