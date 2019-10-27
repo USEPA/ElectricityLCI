1517e096cbfb5dab20f720eefc2e5cc97316a0ad
Jamieson <jamiesom>
2019-10-26 21:11:41 -0400

    Commented out unused code.

--- electricitylci/emissions_other_sources.py
+++ electricitylci/emissions_other_sources.py
@@ -3,14 +3,14 @@ Integration of emissions from data sources that were not included in stewi
 """
 
 import pandas as pd
-from electricitylci.coal_upstream import generate_upstream_coal
+# from electricitylci.coal_upstream import generate_upstream_coal
 from electricitylci.model_config import eia_gen_year
 
 # Import functions for other upstream fuels and include them in this list
 # with the appropriate input parameters.
-UPSTREAM_EMISSION_GENERATORS = [
-    generate_upstream_coal(eia_gen_year),
-]
+# UPSTREAM_EMISSION_GENERATORS = []
+#     generate_upstream_coal(eia_gen_year),
+# ]
 
 
 def integrate_replace_emissions(new_emissions, stewi_emissions):
@@ -79,56 +79,56 @@ def integrate_replace_emissions(new_emissions, stewi_emissions):
     return updated_emissions
 
 
-def combine_upstream_emissions():
-    """
-    Combine upstream emissions generated from a list of functions. Each function
-    generates the upstream emissions from a single fuel type (e.g. coal, NG, etc.)
-    as a dataframe.
+# def combine_upstream_emissions():
+#     """
+#     Combine upstream emissions generated from a list of functions. Each function
+#     generates the upstream emissions from a single fuel type (e.g. coal, NG, etc.)
+#     as a dataframe.
 
-    All dataframes should include the columns:
-        - [ADD COLUMN NAMES]
+#     All dataframes should include the columns:
+#         - [ADD COLUMN NAMES]
 
-    Returns
-    -------
-    DataFrame
-        A single dataframe with emissions upstream of the power plant for all
-        fuel types.
-    """
+#     Returns
+#     -------
+#     DataFrame
+#         A single dataframe with emissions upstream of the power plant for all
+#         fuel types.
+#     """
 
-    upstream = pd.concat(fn for fn in UPSTREAM_EMISSION_GENERATORS)
+#     upstream = pd.concat(fn for fn in UPSTREAM_EMISSION_GENERATORS)
 
-    return upstream
+#     return upstream
 
 
-def add_upstream_emissions(plant_emissions):
-    """
-    Add upstream emissions to emissions at the power plant. The upstream emissions
-    have extra columns.
+# def add_upstream_emissions(plant_emissions):
+#     """
+#     Add upstream emissions to emissions at the power plant. The upstream emissions
+#     have extra columns.
 
-    - Need a fuel type column
-    - Need a life cycle stage column
-    - Only for use when exporting to dataframe (no JSON-LD for now)
+#     - Need a fuel type column
+#     - Need a life cycle stage column
+#     - Only for use when exporting to dataframe (no JSON-LD for now)
 
-    New columns:
-        - Fuel type? (coal/gas/nuclear/solar/wind, etc)
-        - Fuel index code? (e.g. subbit from prb surface)
-        - Stage (e.g. extraction, transportation)
-        - Stage index? (maybe add dictionary in globals.py to map stage names
-        across fuels)
-        -
-    Parameters
-    ----------
-    plant_emissions : DataFrame
-        Facility-level emissions from each power plant.
+#     New columns:
+#         - Fuel type? (coal/gas/nuclear/solar/wind, etc)
+#         - Fuel index code? (e.g. subbit from prb surface)
+#         - Stage (e.g. extraction, transportation)
+#         - Stage index? (maybe add dictionary in globals.py to map stage names
+#         across fuels)
+#         -
+#     Parameters
+#     ----------
+#     plant_emissions : DataFrame
+#         Facility-level emissions from each power plant.
 
-    Returns
-    -------
-    DataFrame
-        The combined emissions from power plant and upstream fuel life cycle.
-    """
+#     Returns
+#     -------
+#     DataFrame
+#         The combined emissions from power plant and upstream fuel life cycle.
+#     """
 
-    upstream_emissions = combine_upstream_emissions()
+#     upstream_emissions = combine_upstream_emissions()
 
-    combined_emissions = pd.concat([plant_emissions, upstream_emissions])
+#     combined_emissions = pd.concat([plant_emissions, upstream_emissions])
 
-    return combined_emissions
+#     return combined_emissions
