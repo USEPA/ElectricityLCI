4d3678761c350e975f14ec7258b0516470143285
WesIngwersen <ingwersen.wesley@epa.gov>
2020-05-28 11:59:02 -0400

    Add to setup and requirements the StEWI release install from github

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     author_email='ingwersen.wesley@epa.gov',
     description='Create life cycle inventory data for regionalized electricity generation, mix of generation, mix of consumption, and distribution to the end-user in the United States.',
     install_requires=[
-        'fedelemflowlist @ git+git://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List@v1.0.2#egg=fedelemflowlist'
+        'fedelemflowlist @ git+git://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List@v1.0.2#egg=fedelemflowlist',
+        'StEWI @ git+git://github.com/USEPA/standardizedinventories@v0.9.2#egg=StEWI',
         'numpy>=1.14',
         'pandas>=0.24',
         'olca-ipc>=0.0.6',
@@ -20,18 +21,16 @@ setup(
         'seaborn>=0.9',
         'sympy>=1.2',
         'xlrd>=1.1',
-        'pyyaml>=5.1',
+        'pyyaml>=5.1'
         ],
     long_description=open('README.md').read(),
     classifiers=[
-        "Development Status :: Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: IDE",
         "Intended Audience :: Science/Research",
         "License :: CC0",
         "Programming Language :: Python :: 3.x",
         "Topic :: Utilities",
-    ],
-    # This does not automatically install these dependencies
-    dependency_links=['https://github.com/USEPA/standardizedinventories/archive/v0,9.zip',
-                      'https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List/archive/master.zip']
+    ]
+
 )
