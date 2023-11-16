1fbd469c35ecc27b0d2e20ed253cbd64ce8e9858
dt-woods <davisler@gmail.com>
2023-11-16 13:09:37 -0500

    update doc; addresses #204

--- electricitylci/bulk_eia_data.py
+++ electricitylci/bulk_eia_data.py
@@ -6,71 +6,52 @@
 ##############################################################################
 # REQUIRED MODULES
 ##############################################################################
-"""Extract hourly real-time EIA data from the bulk-download zip file."""
-
-import pandas as pd
-import json
-from os.path import join
+import logging
 import os
 import zipfile
+
+import pandas as pd
 import requests
-import logging
+
 from electricitylci.globals import paths
 
-logger = logging.getLogger("bulk_eia_data")
+
+##############################################################################
+# MODULE DOCUMENTATION
+##############################################################################
+__doc__ = """This module provides a function, :func:`download_EBA` to
+download the bulk EIA data needed to determine the electricity trading
+between balancing authority areas or between Federal Energy Regulatory
+Commission regions. This electricity trading ultimately decides the
+consumption mix for a given region.
+
+Additional methods are available within this module, but much of it is
+duplicated in eia_io_trading.py.
+
+Last updated: 2023-11-15
+"""
+__all__ = [
+    "ba_exchange_to_df",
+    "download_EBA",
+    "row_to_df",
+]
 
 
 ##############################################################################
 # FUNCTIONS
 ##############################################################################
 def download_EBA():
-    """Add docstring."""
+    """Attempt to download a copy EIA's U.S. Electric System Operating Data
+    from their API site (http://api.eia.gov/bulk/EBA.zip).
+    """
     url = 'http://api.eia.gov/bulk/EBA.zip'
     logging.info(f"Downloading eia bulk data from {url}...")
     r = requests.get(url)
-    os.makedirs(join(paths.local_path, 'bulk_data'), exist_ok=True)
-    output = open(join(paths.local_path, 'bulk_data', 'EBA.zip'), 'wb')
+    os.makedirs(os.path.join(paths.local_path, 'bulk_data'), exist_ok=True)
+    output = open(os.path.join(paths.local_path, 'bulk_data', 'EBA.zip'), 'wb')
     output.write(r.content)
     output.close()
-    logger.info(f"complete.")
-
-
-path = join(paths.local_path, 'bulk_data', 'EBA.zip')
-
-
-# REGION_NAMES = [
-#    'California', 'Carolinas', 'Central',
-#    'Electric Reliability Council of Texas, Inc.', 'Florida',
-#    'Mid-Atlantic', 'Midwest', 'New England ISO',
-#    'New York Independent System Operator', 'Northwest', 'Southeast',
-#    'Southwest', 'Tennessee Valley Authority'
-# ]
-#
-# REGION_ACRONYMS = [
-#    'TVA', 'MIDA', 'CAL', 'CAR', 'CENT', 'ERCO', 'FLA',
-#    'MIDW', 'ISNE', 'NYIS', 'NW', 'SE', 'SW',
-# ]
-#
-# TOTAL_INTERCHANGE_ROWS = [
-#    json.loads(row) for row in raw_txt if b'EBA.TI.H' in row
-# ]
-#
-# NET_GEN_ROWS = [
-#    json.loads(row) for row in raw_txt if b'EBA.NG.H' in row
-# ]
-#
-# DEMAND_ROWS = [
-#    json.loads(row) for row in raw_txt if b'EBA.D.H' in row
-# ]
-#
-# EXCHANGE_ROWS = [
-#    json.loads(row) for row in raw_txt if b'EBA.ID.H' in row
-# ]
-#
-# BA_TO_BA_ROWS = [
-#    row for row in EXCHANGE_ROWS
-#    if row['series_id'].split('-')[0][4:] not in REGION_ACRONYMS
-# ]
+    logging.info(f"complete.")
 
 
 def row_to_df(rows, data_type):
@@ -91,36 +72,26 @@ def row_to_df(rows, data_type):
     """
     tuple_list = []
     for row in rows:
-
-        # "data" is of form:
-        # [['20190214T04Z', -102],
-        # ['20190214T03Z', -107],
-        # ['20190214T02Z', -108],
-        # ['20190214T01Z', -103]]
         try:
-            datetime = pd.to_datetime([x[0] for x in row['data']],
-                                      utc=True, format='%Y%m%dT%HZ')
+            datetime = pd.to_datetime(
+                [x[0] for x in row['data']],
+                utc=True,
+                format='%Y%m%dT%HZ'
+            )
         except ValueError:
             try:
-                datetime = pd.to_datetime([x[0]+":00" for x in row['data']],
-                                          format='%Y%m%dT%H%z')
+                datetime = pd.to_datetime(
+                    [x[0]+":00" for x in row['data']],
+                    format='%Y%m%dT%H%z'
+                )
             except ValueError:
                 continue
         data = [x[1] for x in row['data']]
         region = row['series_id'].split('-')[0][4:]
-
-#        df_data = {
-#            'region': region,
-#            'datetime': datetime,
-#            data_type: data,
-#        }
-#        region_list=[region for x in datetime]
-#        _df = pd.DataFrame(df_data)
-#        tuple_list.append(_df)
-        tuple_data=[x for x in zip([region]*len(datetime), list(datetime), data)]
+        tuple_data = [
+            x for x in zip([region]*len(datetime), list(datetime), data)]
         tuple_list.extend(tuple_data)
-    df=pd.DataFrame(tuple_list, columns=["region", "datetime", data_type])
-#    df = pd.concat(df_list).reset_index(drop=True)
+    df = pd.DataFrame(tuple_list, columns=["region", "datetime", data_type])
 
     return df
 
@@ -129,51 +100,52 @@ def ba_exchange_to_df(rows, data_type='ba_to_ba'):
     """
     Turn rows of a single type from the bulk data text file into a dataframe
     with the region, datetime, and data as columns
+
     Parameters
     ----------
     rows : list
         rows from the EBA.txt file
     data_type : str
         name to use for the data column (e.g. demand or total_interchange)
+
     Returns
     -------
-    dataframe
+    pandas.DataFrame
         Data for all regions in a single df with datatimes converted and UTC
     """
     tuple_list = []
     for row in rows:
-
-        # "data" is of form:
-        # [['20190214T04Z', -102],
-        # ['20190214T03Z', -107],
-        # ['20190214T02Z', -108],
-        # ['20190214T01Z', -103]]
         try:
-            datetime = pd.to_datetime([x[0] for x in row['data']],
-                                      utc=True, format='%Y%m%dT%HZ')
+            datetime = pd.to_datetime(
+                [x[0] for x in row['data']],
+                utc=True,
+                format='%Y%m%dT%HZ'
+            )
         except ValueError:
             try:
-                datetime = pd.to_datetime([x[0]+"00" for x in row['data']],
-                                          format='%Y%m%dT%H%z')
+                datetime = pd.to_datetime(
+                    [x[0]+"00" for x in row['data']],
+                    format='%Y%m%dT%H%z'
+                )
             except ValueError:
                 continue
         data = [x[1] for x in row['data']]
         from_region = row['series_id'].split('-')[0][4:]
         to_region = row['series_id'].split('-')[1][:-5]
-
-#        df_data = {
-#            'from_region': from_region,
-#            'to_region': to_region,
-#            'datetime': datetime,
-#            data_type: data,
-#        }
-        tuple_data = [x for x in zip([from_region]*len(datetime), [to_region]*len(datetime), datetime, data)]
+        tuple_data = [
+            x for x in zip(
+                [from_region]*len(datetime),
+                [to_region]*len(datetime),
+                datetime,
+                data
+            )
+        ]
         tuple_list.extend(tuple_data)
-#        _df = pd.DataFrame(df_data)
-#        df_list.append(_df)
+    df = pd.DataFrame(
+        tuple_list,
+        columns=["from_region", "to_region", "datetime", data_type]
+    )
 
-#    df = pd.concat(df_list).reset_index(drop=True)
-    df=pd.DataFrame(tuple_list, columns=["from_region", "to_region", "datetime", data_type])
     return df
 
 
@@ -181,6 +153,7 @@ def ba_exchange_to_df(rows, data_type='ba_to_ba'):
 # MAIN
 ##############################################################################
 if __name__=="__main__":
+    path = os.path.join(paths.local_path, 'bulk_data', 'EBA.zip')
     try:
         z = zipfile.ZipFile(path, 'r')
         with z.open('EBA.txt') as f:
