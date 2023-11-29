d34230276bdeb863c1e155de56368521171d3d8f
dt-woods <davisler@gmail.com>
2023-11-29 12:56:51 -0500

    addresses #204

--- electricitylci/manual_edits.py
+++ electricitylci/manual_edits.py
@@ -6,80 +6,175 @@
 ##############################################################################
 # REQUIRED MODULES
 ##############################################################################
-import yaml
-from electricitylci.globals import data_dir
 import logging
+import os
+
+import yaml
+
+from electricitylci.globals import data_dir
+
+
+##############################################################################
+# MODULE DOCUMENTATION
+##############################################################################
+__doc__ = """This module provides methods that read a YAML file with
+reassignment or removal instructions for electricity generation inventories.
+For details on the issues these manual edits fix, see the following issues
+on GitHub:
+
+-   https://github.com/USEPA/ElectricityLCI/issues/77
+-   https://github.com/USEPA/ElectricityLCI/issues/121
+
+Referenced by 'create_generation_process_df' in generation.py.
+
+Last updated:
+    2023-11-29
+"""
+__all__ = [
+    "check_for_edits",
+    "manual_edits",
+    "reassign",
+    "remove",
+]
+
+
+##############################################################################
+# GLOBALS
+##############################################################################
+with open(os.path.join(data_dir, "manual_edits.yml"), 'r') as f:
+    manual_edits = yaml.safe_load(f)
 
 
 ##############################################################################
 # FUNCTIONS
 ##############################################################################
-module_logger = logging.getLogger("manual_edits.py")
+def check_for_edits(data, calling_module, calling_function):
+    """Perform manual edits to a given data frame.
 
-with open(f"{data_dir}/manual_edits.yml", "r") as f:
-    manual_edits = yaml.safe_load(f)
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        A data frame in which edits are to made.
+    calling_module : str
+        Module name (e.g., 'generation.py')
+    calling_function : str
+        Function name (e.g., 'create_generation_process_df')
+
+    Returns
+    -------
+    pandas.DataFrame
+        The same data frame sent with manual edits applied.
+    """
+    try:
+        # Pull dictionary of edit entries from YAML.
+        edits_to_make = manual_edits[calling_module][calling_function]
+
+        # Iterate over each entry & check edit type (e.g., remove or reassign)
+        for ed in edits_to_make.keys():
+            logging.info(f"Edits found for {calling_module}.{calling_function}")
+            if edits_to_make[ed]["edit_type"] == "reassign":
+                data = reassign(data, edits_to_make[ed])
+            elif edits_to_make[ed]["edit_type"] == "remove":
+                data = remove(data, edits_to_make[ed])
+            else:
+                logging.warning("Edits found but no handler for function!")
+    except KeyError:
+        logging.info("No manual edits found")
+
+    return data
+
 
 def reassign(data, edit_dict):
+    """Perform value reassignment in a given data frame based on filtering
+    criteria.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        A data frame with columns of data to be reassigned.
+    edit_dict : dict
+        A dictionary with 'filters' key with column:value pairs to be used
+        for filtering rows, 'column_to_reassign' to identify the data frame
+        column where reassignment should occur, 'incoming_value' to find the
+        rows with the wrong value (to be replace), and 'outgoing_value' with
+        the new value to replace the incoming value.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The same data frame received with values matching the filter and
+        reassignment criteria replaced.
+    """
     try:
-        if edit_dict["data_source"]=="yaml":
+        if edit_dict["data_source"] == "yaml":
             logging.info("Re-assigning using data from yaml")
-            # logging.info(f"{edit_dict}")
+
             col = edit_dict["column_to_reassign"]
-            # logging.info(f"{col}")
+            logging.debug(f"{col}")
+
             in_val = edit_dict["incoming_value"]
-            # logging.info(f"{in_val}")
+            logging.debug(f"{in_val}")
+
             out_val = edit_dict["outgoing_value"]
-            # logging.info(f"{out_val}")
-            combined_filter=data[col]==in_val
-            # logging.info(f"{combined_filter}")
+            logging.debug(f"{out_val}")
+
+            # Find all rows associated with the incoming value in the
+            # reassignment column.
+            combined_filter = data[col]==in_val
+
+            # Reduce to only those rows that match each additional filter:
             for filt in edit_dict["filters"].keys():
-                # logging.info(f"Filters are {edit_dict['filters'][filt]}")
-                combined_filter=(
+                logging.debug(
+                    f"Filters for {filt} are {edit_dict['filters'][filt]}")
+                combined_filter = (
                     combined_filter &
                     data[filt].isin(edit_dict["filters"][filt])
                 )
-                # logging.info(f"{combined_filter}")
-            data.loc[combined_filter,col]=out_val
-        return data
-    except KeyError:
-        module_logger.warning("Problem found with manual edit - reassign")
-        return data
+            data.loc[combined_filter, col] = out_val
+    except KeyError as ke:
+        logging.warning("Problem found with manual edit - reassign")
+        logging.warning("%s" % str(ke))
+
+    return data
+
 
 def remove(data, edit_dict):
+    """Drop rows from a data frame based on filtering criteria.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        A data frame from which rows are to be dropped.
+    edit_dict : dict
+        A dictionary including a 'filters' key with column:value pairs to be
+        used to identify rows in the data frame to drop.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The same data frame received, but with rows dropped based on the
+        filter criteria.
+    """
     try:
-        if edit_dict["data_source"]=="yaml":
+        if edit_dict["data_source"] == "yaml":
+            # Find the rows associated with the filter keywords
             logging.info("Removing using data from yaml")
-            combined_filter=None
-            # logging.info(f"{combined_filter}")
+            combined_filter = None
             for filt in edit_dict["filters"].keys():
-                # logging.info(f"Filters are {edit_dict['filters'][filt]}")
+                logging.debug(
+                    f"Filters for {filt} are {edit_dict['filters'][filt]}")
                 if combined_filter is None:
-                    combined_filter=data[filt].isin(edit_dict["filters"][filt])
+                    combined_filter = data[filt].isin(
+                        edit_dict["filters"][filt])
                 else:
-                    combined_filter=(
+                    combined_filter = (
                         combined_filter &
                         data[filt].isin(edit_dict["filters"][filt])
                     )
-                # logging.info(f"{combined_filter}")
-            data = data.loc[~combined_filter,:]
-        return data
-    except KeyError:
-        module_logger.warning("Problem found with manual edit - remove")
-        return data
+            # Drop rows from dataset
+            data = data.loc[~combined_filter, :]
+    except KeyError as ke:
+        logging.warning("Problem found with manual edit - remove")
+        logging.warning("%s" % str(ke))
 
-def check_for_edits(data, calling_module, calling_function):
-    try:
-        edits_to_make=manual_edits[calling_module][calling_function]
-        for ed in edits_to_make.keys():
-            module_logger.info(f"Edits found for {calling_module}.{calling_function}")
-            if edits_to_make[ed]["edit_type"]=="reassign":
-                data=reassign(data,edits_to_make[ed])
-            elif edits_to_make[ed]["edit_type"]=="remove":
-                data=remove(data,edits_to_make[ed])
-            else:
-                logging.info("Edit found but no handler for function")
-        return data
-    except KeyError:
-        module_logger.info("No manual edits found")
-        return data
-    
+    return data
