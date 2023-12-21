328cdadb384eacc943b6b3fe8b72069568f4f8e5
dt-woods <davisler@gmail.com>
2023-12-21 10:56:22 -0500

    addresses #204

--- electricitylci/utils.py
+++ electricitylci/utils.py
@@ -21,13 +21,11 @@ from electricitylci.globals import data_dir
 ##############################################################################
 # MODULE DOCUMENTATION
 ##############################################################################
-__doc__ = """Small utility functions for use throughout the repository."""
+__doc__ = """Small utility functions for use throughout the repository.
 
-
-##############################################################################
-# GLOBALS
-##############################################################################
-module_logger = logging.getLogger("utils.py")
+Last updated:
+    2023-12-21
+"""
 
 
 ##############################################################################
@@ -35,20 +33,20 @@ module_logger = logging.getLogger("utils.py")
 ##############################################################################
 def download_unzip(url, unzip_path):
     """
-    Download a zip file from url and extract contents to a given path.
+    Download and extract contents from a .zip file from a given url to a given
+    path.
 
     Parameters
     ----------
     url : str
-        Valid url to download the zip file
+        Valid URL to download the zip file
     unzip_path : str or path object
         Destination to unzip the data
-
     """
     r = requests.get(url)
     content_type = r.headers["Content-Type"]
     if "zip" not in content_type and "-stream" not in content_type:
-        module_logger.error(content_type)
+        logging.error(content_type)
         raise ValueError("URL does not point to valid zip file")
 
     z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -56,11 +54,39 @@ def download_unzip(url, unzip_path):
 
 
 def find_file_in_folder(folder_path, file_pattern_match, return_name=True):
-    """Add docstring."""
+    """Search a folder for files matching a pattern.
+
+    Parameters
+    ----------
+    folder_path : str
+        An existing directory path.
+    file_pattern_match : list
+        A list of keywords used to match a file name.
+    return_name : bool, optional
+        Whether to return the filename identified in addition to the full path,
+        by default True
+
+    Returns
+    -------
+    str or tuple
+        The file path or, if `return_name` is true, a tuple of the file path
+        and its basename.
+
+    Examples
+    --------
+    >>> import os
+    >>> from electricitylci.globals import paths
+    >>> my_dir = os.path.join(paths.local_folder, 'f923_2016')
+    >>> find_file_in_folder(my_dir, ['2_3_4_5', 'csv'])
+    (
+      '~/electricitylci/f923_2016/                                 \
+       EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision_page_1.csv',
+      'EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision_page_1.csv')
+    """
     files = os.listdir(folder_path)
 
-    # would be more elegent with glob but this works to identify the
-    # file in question
+    # Would be more elegant with glob but this works to identify the
+    # file in question.
     for f in files:
         # modified this so that we can search for multiple strings in the
         # file name - mostly to support different pages of csv files from 923.
@@ -75,10 +101,41 @@ def find_file_in_folder(folder_path, file_pattern_match, return_name=True):
         return (file_path, file_name)
 
 
-def create_ba_region_map(
-    match_fn="BA code match.csv", region_col="ferc_region"
-):
+def create_ba_region_map(match_fn="BA code match.csv",
+                         region_col="ferc_region"):
+    """Generate a pandas series for mapping a region to balancing authority.
 
+    Used in eia860_facilities.py
+
+    Parameters
+    ----------
+    match_fn : str, optional
+        The mapping data file, by default "BA code match.csv"
+    region_col : str, optional
+        The column name from the mapping file associated with the region to be
+        mapped to balancing authority code. For default data file, valid
+        options include, 'ferc_region,' 'eia_region,' and 'Balancing Authority
+        Code,' the latter is trivial as it maps itself.
+        Defaults to "ferc_region."
+
+    Returns
+    -------
+    pandas.Series
+        A series with indices associated with balancing authority codes and
+        values from the requested region column (e.g., ferc_region).
+
+    Examples
+    --------
+    >>> m = create_ba_region_map()
+    >>> m.head()
+    Balancing Authority Code
+    AEC        SE
+    AECI     MISO
+    AVA        NW
+    AZPS       SW
+    BANC    CAISO
+    Name: ferc_region, dtype: object
+    """
     match_path = os.path.join(data_dir, match_fn)
     region_match = pd.read_csv(match_path, index_col=0)
     region_match["Balancing Authority Code"] = region_match.index
@@ -94,22 +151,19 @@ def create_ba_region_map(
         elif 'us' in region_col.lower():
             region_col = 'ferc_region'
         else:
-            module_logger.warning(f"regional_col value is {region_col} - a mapping for this does not exist, using ferc_region instead")
-#                'or "eia_region"')
+            logging.warning(
+                f"regional_col value is {region_col} - "
+                "a mapping for this does not exist, "
+                "using ferc_region instead")
             region_col = 'ferc_region'
-#            raise (
-#                ValueError,
-#                f'regional_col value is {region_col}, but should match "ferc_region" '
-#                'or "eia_region"'
-#            )
-
         map_series = region_match[region_col]
+
     return map_series
 
 
 def fill_default_provider_uuids(dict_to_fill, *args):
     """
-    Fills in UUIDs.
+    Fill UUIDs for default providers.
 
     For default providers in the specified dictionary (dict_to_fill) using any
     number of other dictionaries given in args to find the matching process and
@@ -118,49 +172,50 @@ def fill_default_provider_uuids(dict_to_fill, *args):
 
     Parameters
     ----------
-    dict_to_fill : dictionary
+    dict_to_fill : dict
         A dictionary in the openLCA schema with processes that have
         input exchanges with default provider names provided but not
-        UUIDs
+        UUIDs.
     *args: dictionary
         Any number of dictionaries to search for matching processes
         for the UUIDs
 
     Returns
     -------
-    dictionary
+    dict
         The dict_to_fill input with UUIDs filled in where matching
         processes were found.
     """
     found = False
     dict_list = list(args)
     list_of_dicts = [isinstance(x,dict) for x in dict_list]
-    module_logger.info("Attempting to find UUIDs for default providers...")
+    logging.info("Attempting to find UUIDs for default providers...")
     if all(list_of_dicts):
         for key in dict_to_fill.keys():
             for exch in dict_to_fill[key]['exchanges']:
                 if exch['input'] is True and isinstance(exch['provider'],dict):
-                    found=False
+                    found = False
                     for src_dict in args:
                         for src_key in src_dict.keys():
                             if src_dict[src_key]["name"]==exch["provider"]["name"] and isinstance(src_dict[src_key]["uuid"],str):
                                 exch["provider"]["@id"]=src_dict[src_key]["uuid"]
-                                module_logger.debug(f"UUID for {exch['provider']} found")
+                                logging.debug(
+                                    f"UUID for {exch['provider']} found")
                                 found = True
                                 break;
                         if found:
                             break;
                     if not found:
-                        module_logger.info(f"UUID for {exch['provider']} not found")
+                        logging.info(f"UUID for {exch['provider']} not found")
     else:
-        module_logger.warning(f"All arguments into function must be dictionaries")
+        logging.warning(f"All arguments into function must be dictionaries")
     return dict_to_fill
 
 
 def make_valid_version_num(foo):
     """
-    Strip letters from a string to keep only digits and dots to make
-    the version number valid in olca-schema processes
+    Strip letters from a string to keep only digits and dots in order to make
+    the version number valid in olca-schema processes.
 
     Notes
     -----
@@ -181,15 +236,55 @@ def make_valid_version_num(foo):
 
 
 def set_dir(directory):
-    if not os.path.exists(directory): os.makedirs(directory)
+    """Ensure an output directory exists.
+
+    Notes
+    -----
+    Generates a directory path, if it does not exist.
+
+    Parameters
+    ----------
+    directory : str
+        A path to a directory.
+
+    Returns
+    -------
+    str
+        The path to an existing directory.
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     return directory
 
 
 def join_with_underscore(items):
+    """A helper method to concatenate items together using an underscore.
+
+    If the items are not strings, they are cast to strings.
+    If the items are a dictionary, only the keys are concatenated.
+
+    Parameters
+    ----------
+    items : list, tuple
+        An iterable object with values to be concatenated.
+
+    Returns
+    -------
+    str
+        An underscore joined string of values.
+
+    Examples
+    --------
+    >>> join_with_underscore(['a', 'b', 'c'])
+    'a_b_c'
+    >>> join_with_underscore({'a': 1, 'b': 2, 'c': 3})
+    'a_b_c'
+    >>> join_with_underscore([i for i in range(10)])
+    '0_1_2_3_4_5_6_7_8_9'
+    """
     type_cast_to_str = False
     for x in items:
         if not isinstance(x, str):
-            # raise TypeError("join_with_underscore()  inputs must be string")
             type_cast_to_str = True
     if type_cast_to_str:
         items = [str(x) for x in items]
