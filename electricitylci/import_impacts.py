7f7207722e921eea59f069817ff5b13e087b4077
dt-woods <davisler@gmail.com>
2024-03-08 12:28:08 -0500

    addresses #232
    also removes reliance on model_specs in an attempt to eliminate the over-use of this class in methods and modules that do not need and create more reusable code - addresses #206

--- electricitylci/import_impacts.py
+++ electricitylci/import_impacts.py
@@ -12,7 +12,6 @@ import os
 import pandas as pd
 
 from electricitylci.globals import data_dir
-from electricitylci.model_config import model_specs
 
 
 ##############################################################################
@@ -27,7 +26,7 @@ Hydro & Power Authority generation mix includes 9 per-cent biomass, then U.S.
 that includes balancing authority level inventories for Canadian imports.
 
 Last updated:
-    2024-03-05
+    2024-03-08
 """
 __all__ = [
     "generate_canadian_mixes",
@@ -40,7 +39,7 @@ logger = logging.getLogger("import_impacts")
 ##############################################################################
 # FUNCTIONS
 ##############################################################################
-def generate_canadian_mixes(us_inventory):
+def generate_canadian_mixes(us_inventory, gen_year):
     """Create the Canadian balancing authority inventory data frame.
 
     The inventories are weighted by the percent of each fuel type of
@@ -58,6 +57,8 @@ def generate_canadian_mixes(us_inventory):
     us_inventory: pandas.DataFrame
         A data frame containing flow-level inventory for all fuel categories
         in the United States.
+    gen_year : int
+        The generation year (e.g., 2016).
 
     Returns
     ----------
@@ -97,9 +98,7 @@ def generate_canadian_mixes(us_inventory):
         os.path.join(data_dir, "International_Electricity_Mix.csv"),
     )
     # BUG: missing 2021 and beyond data.
-    import_mix = import_mix.loc[
-        import_mix["Year"] == model_specs.eia_gen_year, :
-    ]
+    import_mix = import_mix.loc[import_mix["Year"] == gen_year, :]
     canadian_mix = import_mix
     canadian_mix["Code"] = canadian_mix["Subregion"].map(canada_subregion_map)
     baa_codes = list(canadian_mix["Code"].unique())
@@ -237,3 +236,95 @@ def generate_canadian_mixes(us_inventory):
     ].map(BA_CODES["EIA_Region"])
 
     return ca_mix_inventory
+
+
+def read_canadian_mix(file_path, pre_process=False):
+    # IN PROGRESS
+    # The original International_ELectricity_Mix.csv has five columns and
+    # need no preprocessing.
+    # - Subregion (str)
+    # - FuelCategory (str)
+    # - Electricity (empty)
+    # - Generation_Ratio (float)
+    # - Year (int)
+    #
+    # If downloading raw from Canadian Energy Futures website,
+    # then it has the following columns and need pre-processing.
+    # - Scenario (str)
+    # - Region (str)
+    # - Variable (str): fuel categories
+    # - Type (str): "PrimaryFuel"
+    # - Year (int)
+    # - Value (float)
+    # - (Unit) only for 2021, should be GWh but we're calculating mix percents
+    #
+    if not os.path.isfile(file_path):
+        raise OSError("Canadian mix file not found! %s" % file_path)
+
+    df = pd.read_csv(file_path)
+    if pre_process:
+        # These steps are taken from NETL workbook, where a pivot table was
+        # implemented to calculate the electricity mix percentages for each
+        # variable (fuel category) from each year and region
+        df = pd.read_csv(file_path)
+
+        # Filter the scenario
+        if 'Current Policy' in df['Scenario'].values:
+            # 2021
+            q_val = 'Current Policy'
+        elif 'Current Measures' in df['Scenario'].values:
+            # 2023
+            q_val = 'Current Measures'
+        elif 'Reference' in df['Scenario'].values:
+            # 2019, 2020
+            q_val = 'Reference'
+        else:
+            raise ValueError("Failed to find reference scenario!")
+        df = df.query("Scenario == '%s'" % q_val).copy()
+
+        # Calculate the generation ratio
+        # The fuel sums
+        a = df.groupby(by=['Year', 'Region', 'Variable']).agg({'Value': "sum"})
+        # The region sums
+        b = df.groupby(by=['Year', 'Region']).agg({'Value': "sum"})
+        b = b.rename(columns={'Value': 'Total'})
+        # Add region sums to region+fuel sums
+        a = a.merge(b, left_index=True, right_index=True)
+        # Calculate percentages
+        a['Generation_Ratio'] = a['Value']/a['Total']
+
+        # TODO:
+        # map fuel category names
+        # map balancing authority names
+
+
+if __name__ == '__main__':
+    #
+    # Because it is assumed that these data files are model results,
+    # the most recent is probably the best option, even for historical
+    # mixes.
+    url20 = "https://www.cer-rec.gc.ca/open/energy/energyfutures2020/electricity-generation-2020.csv"
+    url21 = "https://www.cer-rec.gc.ca/open/energy/energyfutures2021/electricity-generation-2021.csv"
+    url23 = "https://www.cer-rec.gc.ca/open/energy/energyfutures2023/electricity-generation-2023.csv"
+
+    from electricitylci.globals import paths
+    from electricitylci.utils import download
+    from electricitylci.utils import check_output_dir
+
+    # User selects one of the URLs
+    url = url23
+    f_name = os.path.basename(url)
+    out_dir = os.path.join(paths.local_path, "energyfutures")
+    is_dir = check_output_dir(out_dir)
+    if is_dir:
+        out_path = os.path.join(out_dir, f_name)
+        if not os.path.exists(out_path):
+            # Download CSV if not available
+            r = download(url, out_path)
+            if r:
+                logging.info("Downloaded Canadian Energy Futures CSV")
+            else:
+                logging.error(
+                    "Failed to download Canadian Energy Future CSV: %s" % url)
+        if os.path.exists(out_path):
+            df = pd.read_csv(out_path)
