28d385e776345cee0396c4cf7b94daa3a01ac869
dt-woods <davisler@gmail.com>
2024-08-19 09:46:14 -0400

    make it easier to find the JSON-LD at end of run

--- electricitylci/main.py
+++ electricitylci/main.py
@@ -275,6 +275,9 @@ if __name__ == "__main__":
     except Exception as e:
         log.error("Crashed on main!\n%s" % str(e))
     else:
-        log.info("Finished!")
+        log.info(
+            "Finished!\n"
+            "You can find your JSON-LD in this folder: %s" % output_dir
+        )
     finally:
         log.handlers[0].doRollover()
