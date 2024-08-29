2ac9ac853a49b0989ca70f6361979fde311dc6d7
dt-woods <davisler@gmail.com>
2024-08-29 16:17:44 -0400

    remove old version number from fedelemflowlist, just grad latest; addresses #196

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     author_email='ingwersen.wesley@epa.gov',
     description='Create life cycle inventory data for regionalized electricity generation, mix of generation, mix of consumption, and distribution to the end-user in the United States.',
     install_requires=[
-        'fedelemflowlist @ git+https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List@v1.2.0#egg=fedelemflowlist',
-        'StEWI @ git+https://github.com/USEPA/standardizedinventories@v1.1.2#egg=StEWI',
-        'scipy>=1.10'
+        'fedelemflowlist @ git+https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List#egg=fedelemflowlist',
+        'StEWI @ git+https://github.com/USEPA/standardizedinventories#egg=StEWI',
+        'scipy>=1.10',
         ],
     long_description=open('README.md').read(),
     classifiers=[
