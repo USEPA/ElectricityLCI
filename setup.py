ad3ecaa47b43d2a6720763c441b15d76858f1824
dt-woods <davisler@gmail.com>
2025-05-01 14:12:57 -0400

    clean up setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='ElectricityLCI',
+    name='electricitylci',
     version='2.0.0',
     packages=['electricitylci'],
     package_data={
