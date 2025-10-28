77b2fe2d642278276522731dd4bb0274333b768c
dt-woods <davisler@gmail.com>
2025-10-28 11:01:31 -0400

    update affiliation; addresses #316
    The citation now matches the entry on EDX. Update fedelemenflowlist dependency URL. Also update the Pypi classifiers

--- setup.py
+++ setup.py
@@ -16,23 +16,27 @@ setup(
                            "output/.gitignore",
                            ]
     },
-    url='https://github.com/USEPA/ElectricityLCI',
+    url='https://github.com/NETL-RIC/ElectricityLCI',
     license='CC0',
-    author='Matt Jamieson, Wesley Ingwersen, Greg Schively, TJ Ghosh, Ben Young, Troy Hottle',
-    author_email='ingwersen.wesley@epa.gov',
+    author='Tyler W. Davis, Matthew Jamieson, Wesley W. Ingwersen, Greg Schivley, Ben Young, Tapajyoti Ghosh, Jing Li, Shirley Sam, Daniel Lee Young, Michael Srocka, and Troy A. Hottle',
+    author_email='matthew.jamieson@netl.doe.gov',
     description='Create life cycle inventory data for regionalized electricity generation, mix of generation, mix of consumption, and distribution to the end-user in the United States.',
     install_requires=[
-        'fedelemflowlist @ git+https://github.com/USEPA/Federal-LCA-Commons-Elementary-Flow-List#egg=fedelemflowlist',
+        'fedelemflowlist @ git+https://github.com/FLCAC-admin/Federal-LCA-Commons-Elementary-Flow-List#egg=fedelemflowlist',
         'StEWI @ git+https://github.com/USEPA/standardizedinventories#egg=StEWI',
         'scipy>=1.10',
         ],
     long_description=open('README.md').read(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Environment :: IDE",
+        "Environment :: Console",
         "Intended Audience :: Science/Research",
         "License :: CC0",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.x",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
     ]
 )
